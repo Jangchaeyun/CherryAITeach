a5527641bce50b98142dfdbcfba9a2fb1c45544d
Jangchaeyun <zzangsally@naver.com>
2025-06-10 22:15:07 +0900

    06010 AI Companion Conversation

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,6 +3,7 @@ import {redirect} from "next/navigation";
 import {getSubjectColor} from "@/lib/utils";
 import {getCompanion} from "@/lib/actions/companion.action";
 import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 
 interface CompanionSessionPageProps {
   params: Promise<{id: string}>;
@@ -10,8 +11,11 @@ interface CompanionSessionPageProps {
 
 const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
   const { id } = await params;
-  const { name, subject, title, topic, duration } = await getCompanion(id);
+
+  const companion = await getCompanion(id);
   const user = await currentUser();
+
+  const { name, subject, title, topic, duration } = companion;
 
   if (!user) redirect((`/sign-in`));
   if (!name) redirect(('/companions'))
@@ -41,6 +45,14 @@ const CompanionSession = async ({ params }: CompanionSessionPageProps) => {
             {duration} minutes
           </div>
         </article>
+        <CompanionComponent
+            {...companion}
+            companionId={id}
+            userName={user.firstName!}
+            userImage={user.imageUrl!}
+        />
+
+      {/*  name, subject, title, topic, duration */}
       </main>
   )
 };
